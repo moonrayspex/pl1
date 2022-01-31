7753ca54b2d93638260a5226f2cfd3a7f4bd392c
Rachel <89171396+moonrayspex@users.noreply.github.com>
2022-01-31 16:14:27 -0500

    Update storymap.js

--- story/storymap.js
+++ story/storymap.js
@@ -23,8 +23,8 @@ function initMap() {
         destination: { lat: 53.873207404769104, lng: 10.746151065673029},
         //Start at Hauptbahnhof - train station, end at Hotel Arnimsruh
         waypoints: [
-            {location: {placeID: "ChIJrS2izlYJskcRCN7u5pIYUOo"}},
-            {location: {placeID: "ChIJQZcWNFcJskcRDuo5SSNFoLw"}},
+            //{location: {placeID: "ChIJrS2izlYJskcRCN7u5pIYUOo"}},
+            //{location: {placeID: "ChIJQZcWNFcJskcRDuo5SSNFoLw"}},
             {location: { lat: 53.8666335971181, lng: 10.685774984709477}},
             {location: { lat: 53.868524126948884, lng: 10.685739812661957}},
             {location: { lat: 53.866277147735865, lng: 10.688176136906055}},
@@ -33,7 +33,7 @@ function initMap() {
             {location: { lat: 53.86595211672861, lng: 10.691594364528683}},
             {location: {lat: 53.89624449801269, lng: 10.71131512688614}},
             {location: {lat: 53.87029370662248, lng: 10.74457243072382}},
-            {location: {lat: 53.873017004571835, lng: 10.745883695830639}},
+            {location: {lat: 53.873017004571835, lng: 10.745883695830639}}
                       ],
         // Note that Javascript allows us to access the constant
         // using square brackets and a string value as its
